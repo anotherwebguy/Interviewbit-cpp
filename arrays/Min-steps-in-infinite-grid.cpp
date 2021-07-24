17187eb7fc93935b6aa9fbc8b79f8cdb37a42bac
Mohit singh <mohitrajputms123@gmail.com>
2021-07-24 15:08:02 +0530

    Create Min-steps-in-infinite-grid.cpp

---
+++ arrays/Min-steps-in-infinite-grid.cpp
@@ -0,0 +1,9 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+int n = A.size();
+int steps=0;
+for(int i=0;i<n-1;i++){
+    steps += max(abs(A[i]-A[i+1]),abs(B[i]-B[i+1]));
+}
+return steps;
+
+}
