65b9bcac758756b517c540bcc5fe3ab16ecd8189
Mohit singh <mohitrajputms123@gmail.com>
2021-07-27 12:42:20 +0530

    Create Maximum-consecutive-gap.cpp

---
+++ arrays/Maximum-consecutive-gap.cpp
@@ -0,0 +1,10 @@
+int Solution::maximumGap(const vector<int> &A) {
+if(A.size()<2) return 0;
+vector<int> B = A;
+sort(B.begin(),B.end());
+int max = 0;
+for(int i=0;i<B.size()-1;i++){
+    if(B[i+1]-B[i] > max)
+    max = B[i+1]-B[i];
+}
+return max;
