0dee02187b9250ce734cf918b8c36c823af09d76
Mohit singh <mohitrajputms123@gmail.com>
2021-09-22 20:13:41 +0530

    Create implement strstr.cpp

---
+++ Strings/implement strstr.cpp
@@ -0,0 +1,38 @@
+int soln(string A,string B){
+
+int n = A.size();
+
+int m = B.size();
+
+int ans = -1;
+
+for(int i=0;i<n;i++)
+
+{
+
+int j = 0;
+
+int k = i;
+
+while(A[k]==B[j])
+
+{
+
+if(j==m-1)
+
+{
+
+return i;
+
+}
+
+j++;
+
+k++;
+
+}
+
+}
+
+return ans;
+}
