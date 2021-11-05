18596180fde120ec9b78b54c201d2825210afd29
Mohit singh <mohitrajputms123@gmail.com>
2021-11-05 11:20:31 +0530

    Create List Cycle.cpp

---
+++ LinkedList/List Cycle.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::detectCycle(ListNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    ListNode *slow=A, *fast=A;
+    slow = slow->next;
+    fast = fast->next->next;
+    while(slow && fast && fast->next){
+        if(slow==fast) break;
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    if(slow!=fast) return NULL;
+    slow=A;
+    while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    return slow;
+}
